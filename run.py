ff76d0fbd8eafe2ed485ef0d0706d25eea404b75
kai-the-ghost <thirudemon817@gmail.com>
2026-06-06 19:22:12 +0530

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ import os
 os.system('uvicorn main:app --reload --host 0.0.0.0 --port 8000 &')
 time.sleep(2)
 print("server is up")
-os.system('firefox templates/index.html')
+os.system('firefox index.html')
 print("everything is working")
 
